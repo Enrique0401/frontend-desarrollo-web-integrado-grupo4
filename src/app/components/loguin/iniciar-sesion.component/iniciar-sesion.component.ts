9a8078c478bd211bc7d41f1e3f230b2fd822eac1
E Lujan Z <u23273066@utp.edu.pe>
2026-07-14 18:36:30 -0500

    Cambio

--- src/app/components/loguin/iniciar-sesion.component/iniciar-sesion.component.ts
+++ src/app/components/loguin/iniciar-sesion.component/iniciar-sesion.component.ts
@@ -52,9 +52,6 @@ export class IniciarSesionComponent {
 
     this.authService.login(credenciales).subscribe({
       next: (respuesta: AuthResponse) => {
-        //console.log('Conexion exitosa a la BD! Token:', respuesta.token);
-        console.log('El rol detectado es:', respuesta.rol);
-
         this.cargando.set(false);
         this.redirigirPorRol(respuesta.rol);
       },
